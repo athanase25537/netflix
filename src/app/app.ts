95e63158ebc8e4a744c77658806d93caaab16c97
athanase25537 <andriamasyathanase.nazy@gmail.com>
2025-07-09 19:30:04 +0300

    first commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Main } from "./components/main/main";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Main],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
