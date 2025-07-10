eff63b974f65f8f05ee61698b87fd4ef30540ec7
athanase25537 <andriamasyathanase.nazy@gmail.com>
2025-07-10 14:05:10 +0300

    feat: cardReason

---
+++ src/app/components/card-reason/card-reason.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-reason',
+  imports: [],
+  templateUrl: './card-reason.html',
+  styleUrl: './card-reason.scss'
+})
+export class CardReason {
+
+}
