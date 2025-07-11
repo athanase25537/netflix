49dd8ad55dad4c7dda3bf879a5cd10b255b34aab
athanase25537 <andriamasyathanase.nazy@gmail.com>
2025-07-11 14:15:45 +0300

    update: faq fonctionnel

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,11 +2,11 @@ import { Component } from '@angular/core';
 import { Main } from "./components/main/main";
 import { Cards } from "./components/cards/cards";
 import { CardReasons } from "./components/card-reasons/card-reasons";
-import { Faq } from "./components/faq/faq";
+import { Faqs } from './components/faqs/faqs';
 
 @Component({
   selector: 'app-root',
-  imports: [Main, Cards, CardReasons, Faq],
+  imports: [Main, Cards, CardReasons, Faqs],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
