32d45da81097492b91af7ab3ea3c4356a281f2cc
athanase25537 <andriamasyathanase.nazy@gmail.com>
2025-07-11 14:46:27 +0300

    add data for card-mai

--- src/app/services/card-reason-service.ts
+++ src/app/services/card-reason-service.ts
@@ -13,21 +13,21 @@ export class CardReasonService {
     ),
 
     new CardReasonModel(
-      "Regardez Netflix sur votre TV",
-      "Regardez Netflix sur votre Smart TV, PlayStation, Xbox, Chromecast, Apple TV, lecteur Blu-ray et bien plus.",
-      "fa-solid fa-desktop"
+      "Téléchargez vos séries pour les regarder hors connexion",
+      "Enregistrez vos programmes préférés et ayez toujours quelque chose à regarder.",
+      "fa-solid fa-download"
     ),
 
     new CardReasonModel(
-      "Regardez Netflix sur votre TV",
-      "Regardez Netflix sur votre Smart TV, PlayStation, Xbox, Chromecast, Apple TV, lecteur Blu-ray et bien plus.",
-      "fa-solid fa-desktop"
+      "Où que vous soyez",
+      "Regardez des films et séries en accès illimité sur votre TV, smartphone, tablette et ordinateur.",
+      "fa-solid fa-magnifying-glass"
     ),
 
     new CardReasonModel(
-      "Regardez Netflix sur votre TV",
-      "Regardez Netflix sur votre Smart TV, PlayStation, Xbox, Chromecast, Apple TV, lecteur Blu-ray et bien plus.",
-      "fa-solid fa-desktop"
+      "Créez des profils pour les enfants",
+      "Les enfants découvrent de nouvelles aventures et retrouvent leurs personnages préférés dans un espace bien à eux, déjà inclus dans votre abonnement.",
+      "fa-solid fa-face-smile-beam"
     ),
   ]
 
